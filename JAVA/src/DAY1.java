ac533ac80c7b3a618e321d63b3fc8c229785b1e3
Sumit Verma <987sumitverma@gmail.com>
2023-07-30 20:54:21 +0530

    hello world

---
+++ JAVA/src/DAY1.java
@@ -0,0 +1,9 @@
+public class DAY1 {
+
+    //main method is where the program starts its execution
+
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+    }
+}
+//lefkl
